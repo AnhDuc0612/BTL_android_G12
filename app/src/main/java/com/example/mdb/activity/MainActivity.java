14d0cca5863e3c33e9fc463d4a643e722d99e38c
Duc <ducc06122002@gmail.com>
2024-04-12 08:35:02 +0700

    first commit

--- app/src/main/java/com/example/mdb/activity/MainActivity.java
+++ app/src/main/java/com/example/mdb/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mdb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +13,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fabAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fabAdd=findViewById(R.id.btnUpload);
+        fabAdd=findViewById(R.id.fab);
 
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
